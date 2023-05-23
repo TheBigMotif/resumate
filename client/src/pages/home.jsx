28ed372d989b735296466c501ed019bf000289eb
Mauricio-tech <mauricio.trevinon91@gmail.com>
2023-05-23 00:14:20 -0600

    Cleaning code, created data folder for easy reading

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,35 +1,5 @@
-const posts = [
-  {
-    id: 1,
-    title: 'ResumeGenie',
-    Emoji: '🧞‍♂️',
-    href: '#',
-    description:
-      'Unlock your career potential with ResumeGenie. Answer 15 simple questions, and watch as our AI-powered platform creates professional resumes tailored specifically to your skills and experience, helping you stand out in your job search.',
-    buttonText: 'Boost your career 💼',
-    buttonGradient: 'from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500',
-  },
-  {
-    id: 2,
-    title: 'FinGenius',
-    Emoji: '💸',
-    href: '#',
-    description:
-      'Take charge of your finances with FinGenius. Input your numbers and let our AI-driven platform create customized finance templates to save money and reach your financial goals.',
-    buttonText: 'Start organizing 📈',
-    buttonGradient: 'from-purple-500 to-indigo-500 hover:from-red-500 hover:to-yellow-500',
-  },
-  {
-    id: 3,
-    title: 'Dating Booster',
-    Emoji: '❤️‍🔥',
-    href: '#',
-    description:
-      'Share details about your crush and let our AI-powered platform work its magic, offering the ideal recipe to make them fall deeply in love with you. Create your love story with confidence.',
-    buttonText: 'Spark connections ✨',
-    buttonGradient: 'from-pink-500 to-yellow-500 hover:from-purple-500 hover:to-indigo-500',
-  },
-];
+import { posts } from '../data/data.js';
+
 
 export default function Models() {
   return (
