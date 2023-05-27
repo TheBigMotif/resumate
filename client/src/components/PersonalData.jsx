8f8eac88adb4ad432faeee547154a25e9f8937a2
Aaron Agraz <aaron_agraz@hotmail.com>
2023-05-27 12:30:14 -0600

    Modified openaiController

--- client/src/components/PersonalData.jsx
+++ client/src/components/PersonalData.jsx
@@ -1,4 +1,18 @@
+import React, { useState } from "react";
+import { generatetext } from "../../../server/controllers/openaiController.js";
+
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    generatetext(inputValue);
+    // Here you can send the inputValue to OpenAI
+  };
   return (
     <div className="px-24 bg-gray-50">
       <div className="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3">
@@ -12,7 +26,10 @@ const App = () => {
           </p>
         </div>
 
-        <form className="my-24 bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2 md:pl-12">
+        <form
+          onSubmit={handleSubmit}
+          className="my-24 bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2 md:pl-12"
+        >
           <div className="px-4 py-6 sm:p-8">
             <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-3">
@@ -24,10 +41,12 @@ const App = () => {
                 </label>
                 <div className="mt-2">
                   <input
+                    value={inputValue}
+                    onChange={handleChange}
                     type="text"
                     name="first-name"
                     id="first-name"
-                    placeholder="Mauricio"
+                    placeholder="First name"
                     autoComplete="given-name"
                     className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -221,7 +240,7 @@ const App = () => {
               </div>
             </div>
           </div>
-          {/* <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+          <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
             <button
               type="button"
               className="text-sm font-semibold leading-6 text-gray-900 rounded-lg px-8 py-3 shadow-lg"
@@ -229,12 +248,12 @@ const App = () => {
               Cancel
             </button>
             <button
-              type="button"
+              type="submit"
               className="text-sm font-semibold leading-6 text-gray-50 rounded-lg px-8 py-3 shadow-lg bg-gradient-to-r from-purple-500 to-indigo-500 hover:from-red-500 hover:to-yellow-500 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Save
             </button>
-          </div> */}
+          </div>
         </form>
       </div>
     </div>
